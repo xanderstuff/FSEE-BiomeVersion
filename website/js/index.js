0c7586390557ebe99b044eacd9f96bdb3c00f64d
FokaStudio <foka.studio.mc@gmail.com>
2022-08-09 01:26:05 +0200

    😳 Lemme just push this before I break some shit

---
+++ website/js/index.js
@@ -0,0 +1,13 @@
+window.onload = choosePic;
+
+var Thumbnails = new Array("snow","barrens","stronghold","village");
+var body= document.getElementById('random-bg');
+function choosePic() {
+    var randomNum = Math.floor(Math.random() * Thumbnails.length);
+    document.getElementById('random-bg').style.backgroundImage = "linear-gradient(rgba(0, 0, 0, 0.7), rgba(0, 0, 0, 0.7)), url(website/assets/screenshots/"+ Thumbnails[randomNum] + ".png)";
+}
+
+particlesJS.load('random-bg', '../website/assets/particles/index.json', function() {
+    console.log('callback - particles.js config loaded');
+});
+
