09c74cff4d71ddd41a4ac798f2c1a0d610bae5fc
FokaStudio <foka.studio.mc@gmail.com>
2022-09-09 21:10:47 +0200

    ✨ Unf*ck the website on mobile

--- website/js/downloads.js
+++ website/js/downloads.js
@@ -1,4 +1,5 @@
-particlesJS.load('hero-image', '../website/assets/particles/about.json', function() {
-    console.log('callback - particles.js config loaded');
-});
-
+fetch('https://api.github.com/repos/ChocolateLoverRaj/canvideo/commits?per_page=1')
+	.then(res => res.json())
+	.then(res => {
+		document.getElementById('commitmsg').innerHTML = res[0].commit.message
+})
