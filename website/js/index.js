d5c4f70220a0197ccae3dad2b4f2aaddf06bd2f1
FokaStudio <foka.studio.mc@gmail.com>
2022-09-25 17:48:23 +0200

    ✨ Slight improvements to website

--- website/js/index.js
+++ website/js/index.js
@@ -1,6 +1,6 @@
 window.onload = choosePic;
 
-var Thumbnails = new Array("snow","barrens","stronghold","village", "frosted_flesh", "islands");
+var Thumbnails = new Array("snow","barrens","stronghold","village", "frosted_flesh", "islands", "den", "hallow");
 var body= document.getElementById('random-bg');
 function choosePic() {
     var randomNum = Math.floor(Math.random() * Thumbnails.length);
