66f57b63d94a9d746824ab80554af64e4ecbcbb0
FokaStudio <foka.studio.mc@gmail.com>
2022-09-25 19:45:40 +0200

    🐛 Maybe (?) fix website

--- website/js/index.js
+++ website/js/index.js
@@ -1,3 +1,7 @@
+particlesJS.load('random-bg', '../website/assets/particles/index.json', function() {
+    console.log('callback - particles.js config loaded');
+});
+
 window.onload = choosePic;
 
 var Thumbnails = new Array("snow","barrens","stronghold","village", "frosted_flesh", "islands", "den", "hallow");
@@ -6,8 +10,3 @@ function choosePic() {
     var randomNum = Math.floor(Math.random() * Thumbnails.length);
     document.getElementById('random-bg').style.backgroundImage = "linear-gradient(rgba(0, 0, 0, 0.7), rgba(0, 0, 0, 0.7)), url(website/assets/screenshots/"+ Thumbnails[randomNum] + ".png)";
 }
-
-particlesJS.load('random-bg', '../website/assets/particles/index.json', function() {
-    console.log('callback - particles.js config loaded');
-});
-
